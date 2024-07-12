c5bd9da21d6d32bb98148cd249b4e9e6a58d1051
Sajawal Sheraz <hello.sajawal@gmail.com>
2024-07-13 00:57:52 +0500

    finish the hero and navigation sections

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useLocation } from "react-router-dom";
+import { disablePageScroll, enablePageScroll } from "scroll-lock";
 import { brainwave } from "../assets";
 import MenuSvg from "../assets/svg/MenuSvg";
 import { navigation } from "../constants";
@@ -11,11 +12,17 @@ const Header = () => {
   const toggleNavigation = () => {
     if (openNavigation) {
       setOpenNavigation(false);
+      enablePageScroll();
     } else {
       setOpenNavigation(true);
+      disablePageScroll();
     }
   };
   const handleClick = () => {
+    if (!openNavigation) {
+      return;
+    }
+    enablePageScroll();
     setOpenNavigation(false);
   };
   return (
